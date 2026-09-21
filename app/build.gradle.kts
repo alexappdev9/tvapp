0eb9d2efa39ee167e4a8f8d0779208caffd01bb8
alexappdev9 <sharma.316@gmail.com>
2026-09-22 01:35:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@ android {
     namespace = "com.tvapp.cinetv"
     compileSdk = 35
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.tvapp.cinetv"
         minSdk = 23
