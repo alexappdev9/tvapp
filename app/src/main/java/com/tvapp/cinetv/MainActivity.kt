935b40d5fbcefa55ea48a49bbfaf92d7a666f68d
alexappdev9 <sharma.316@gmail.com>
2026-09-23 01:42:44 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tvapp/cinetv/MainActivity.kt
+++ app/src/main/java/com/tvapp/cinetv/MainActivity.kt
@@ -473,7 +473,11 @@ class MainActivity : Activity() {
                     )
 
                     newWebView.bringToFront()
-
+android.widget.Toast.makeText(
+    this@MainActivity,
+    "PLAYER WINDOW CREATED",
+    android.widget.Toast.LENGTH_LONG
+).show()
                     val transport =
                         resultMsg.obj as WebView.WebViewTransport
 
